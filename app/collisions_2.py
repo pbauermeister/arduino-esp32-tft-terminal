d9d46e17b8095b774cb6108f05e6946ab3a5b00c
Pascal Bauermeister <pascal.bauermeister@depsys.com>
2024-01-02 23:30:46 +0100

    Remove old collision and add new variant of collision_s_.

---
+++ app/collisions_2.py
@@ -0,0 +1,11 @@
+from app.collisions import Collisions
+
+
+class Collisions_2(Collisions):
+
+    def set_collisions_params(self) -> None:
+        self.nb = 4
+        self.radius_min = 4
+        self.radius_max = 22
+        self.v_min = 100.0
+        self.v_max = 1000.0
